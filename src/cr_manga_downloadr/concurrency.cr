04a268b801c920fae69fefa6784fec0c8491de3e
Fabio Akita <fabioakita@gmail.com>
2016-06-08 18:43:15 -0300

    updating to use the newest version of fiberpool

--- src/cr_manga_downloadr/concurrency.cr
+++ src/cr_manga_downloadr/concurrency.cr
@@ -6,14 +6,12 @@ module CrMangaDownloadr
 
     def fetch(collection : Array(A)?, &block : A, C? -> Array(B)?) : Array(B)?
       results = [] of B
-      Fiberpool.pool(@config.download_batch_size) do
-        if (collection.try(&.size) || 0) > 0
-          item = collection.try(&.pop)
+      if collection
+        pool = Fiberpool.new(collection, @config.download_batch_size)
+        pool.run do |item|
           engine = @turn_on_engine ? C.new(@config.domain, @config.cache_http) : nil
           reply = block.call(item as A, engine)
           results.concat(reply.try(&.flatten) as Array(B))
-        else
-          raise Fiberpool::BreakException.new
         end
       end
       results
